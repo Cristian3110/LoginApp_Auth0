7c5a07d8f0eb3cdc8df789f89bed5bc204b9a485
Makoto3110 <cristiancarrillo311087@gmail.com>
2020-07-05 05:05:03 -0400

    configuración de recarga de página

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from './service/auth.service';
 
 @Component({
@@ -6,7 +6,9 @@ import { AuthService } from './service/auth.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'loginApp';
   constructor(private auth: AuthService) {}
+
+  ngOnInit() {}
 }
